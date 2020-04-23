e49aad0c51c3792cb771cb22293b7f95ef10ced4
Donutson <46778033+Donutson@users.noreply.github.com>
2020-04-23 02:40:22 +0000

    LCD add
    Challenge code 3

---
+++ lcd.py
@@ -0,0 +1,148 @@
+#-------------------------------------------------------------------------------
+# Name:        lcd
+# Purpose:		resolution du challenge code organise par
+#				Facebook Develoer Circle Abidjan dans la periode du 2020/04/[22-26]
+#
+# Author:      Nelson
+#
+# Created:     22/04/2020
+# Copyright:   (c) HP 2020
+# Licence:     Creative commons
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+import sys
+
+class Numbers:
+
+	def zero(s):
+
+		hor = ' ' + '-'*s + ' '
+		ver = ('|' + ' '*s + '|\n')*s
+
+		return hor + '\n' + ver + '\n' + ver + hor +'\n'
+
+	def one(s):
+
+		hor = ' '*(s+2)+'\n'
+		ver = ('  |'+' '*(s-1)+'\n')*s
+
+		return hor + ver + hor +ver+hor
+
+	def two(s):
+		hor = ' '+'-'*s+' \n'
+		ver1 = (' '*(s+1)+'|\n')*s
+		ver2 = ('|'+' '*(s+1)+'\n')*s
+
+		return hor + ver1 + hor + ver2 + hor
+
+	def three(s):
+		hor = ' '+'-'*s+' \n'
+		ver = (' '*(s+1)+'|\n')*s
+		
+		return hor + ver + hor + ver + hor
+
+	def four(s):
+		edge = ' '*(s+2)+'\n'
+		ver1 = ('|'+' '*s+'|\n')*s+' '+'-'*s+' \n'
+		ver2 = (' '*(s+1)+'|\n')*s
+
+		return edge + ver1 + ver2 + edge
+
+	def five(s):
+		hor = ' '+'-'*s+' \n'
+		ver2 = (' '*(s+1)+'|\n')*s
+		ver1 = ('|'+' '*(s+1)+'\n')*s
+
+		return hor + ver1 + hor + ver2 + hor
+		
+	def six(s):
+		 hor = ' '+'-'*s+' \n'
+		 ver1 = ('|'+' '*(s+1)+'\n')*s
+		 ver2 = ('|'+' '*s+'|\n')*s
+
+		 return hor + ver1 + hor + ver2 +hor
+
+	def seven(s):
+		hor = ' '+'-'*s+' \n'
+		ver = (' '*(s+1)+'|\n')*s
+
+		return hor + ver + ' '*(s+2) + '\n' + ver + ' '*(s+2) + '\n'
+
+	def eight(s):
+		hor = ' '+'-'*s+' \n'
+		ver = ('|'+' '*s+'|\n')*s
+
+		return hor + ver + hor + ver + hor
+
+	def nine(s):
+		hor = ' '+'-'*s+' \n'
+		ver1 = ('|'+' '*s+'|\n')*s
+		ver2 = (' '*(s+1)+'|\n')*s
+
+		return hor + ver1 + hor + ver2 + hor 
+
+	@staticmethod
+	def getnumber(number, s):
+		
+		if number == 0:
+			return Numbers.zero(s)
+
+		if number == 1:
+			return Numbers.one(s)
+
+		if number == 2:
+			return Numbers.two(s)
+
+		if number == 3:
+			return Numbers.three(s)
+
+		if number == 4:
+			return Numbers.four(s)
+
+		if number == 5:
+			return Numbers.five(s)
+
+		if number == 6:
+			return Numbers.six(s)
+
+		if number == 7:
+			return Numbers.seven(s)
+
+		if number == 8:
+			return Numbers.eight(s)
+
+		if number == 9:
+			return Numbers.nine(s)
+
+def showlcd(s, n):
+
+	schema = []
+	nbrows = 2*s + 3
+	nbdigits = len(str(n))
+	screen = ''
+
+	for digit in str(n):
+
+		number = Numbers.getnumber(int(digit), s)
+		number = number.split('\n')
+		number.pop()
+
+		schema.append(number)
+
+	for i in range(nbrows):
+		for j in range(nbdigits-1):
+			screen += schema[j][i] + ' '
+		screen += schema[nbdigits-1][i] + '\n'
+	
+	print(screen)
+
+def main():
+	args = sys.argv[1:-2]
+	for i in range(0, len(args), 2):
+		s, n = int(args[i]), int(args[i+1])
+		
+		showlcd(s,n)
+
+if __name__ == '__main__':
+	main()
